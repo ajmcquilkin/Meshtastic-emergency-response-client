34b4fabe4a1d5357243188f3bff0b47d7e6b3dc5
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-10-29 15:14:38 -0400

    function layout for node/graph initialization

--- src-tauri/src/graph_init.rs
+++ src-tauri/src/graph_init.rs
@@ -1,8 +1,37 @@
 mod graph;
 
-// TODO: initialize empty graph (store in centralized location)
+/*
+* This module handles all of the graph initialization and data flow.
+* Ideally, we'll call its functions to create a graph, run algorithms
+* on it, and return the result to the frontend.
+*/
 
-// include functionality for adding a node to a graph (as more nodes come online)
-// scan for nodes?
+// TODO: Questionable use of global (will need an unsafe block to get)
+static mut GRAPH: Graph = empty;
 
-// update hashmaps when update graphs
+/*
+* This function initializes a graph from all available nodes, storing
+* node information with MAC addresses as keys.
+ */
+pub fn initialize_graph_from_data() -> Graph {}
+
+/*
+* Adds a single node and associated edges/weights to the graph
+*/
+pub fn add_node_to_graph() -> Graph {}
+
+/*
+* This should trigger a fast graph update, adding any additional nodes and updating node location data
+*/
+pub fn update_graph() -> Graph {}
+
+/*
+* Note: this function runs algorithms on a copy of the graph, maintaining the graph state that
+* existed at call time.
+*/
+pub fn run_algorithms_on_graph() -> Graph {}
+
+/*
+* This function serializes and sends graph data to the frontend
+*/
+pub fn export_data_to_frontend() -> i32 {}
--- src-tauri/src/node_init.rs
+++ src-tauri/src/node_init.rs
@@ -1,19 +1,28 @@
 extern crate mac_address;
 mod graph;
 
-// initialize a node from hardware with a mac address
-// TODO: clarify name return vs node initialization for a single node (these are diff behaviors)
-// TODO: allow alternate naming convention if mac addr flow fails
-// Assumption: MAC addresses are unique within the node network
-use mac_address::get_mac_address;
-pub fn node_name_init() -> String {
-    let mac_addr = get_mac_address() {
-        Some (n: String) => n,
-        None => panic!("Initialization process failed to get device mac address"),
-    };
-}
+/*
+* This module handles the fine implementation details of node initialization;
+* e.g. name assignment (with defaults), and assignment of location data.
+*/
 
-fn main() {
-    //testing
-    io::stdout("Hello world");
-}
+/*
+* This function will assign each node a name from it's MAC address. If no address is
+* available, it will alert the user and assign a default name.
+*/
+pub fn assign_node_name() -> String {}
+
+/*
+* This function will use the node's location data to generate edge weights for the graph
+*/
+pub fn generate_edge_weights() -> Graph {}
+
+/*
+* This function will scan an individual node, and update its edge weights accordingly
+*/
+pub fn update_individual_node_data() -> Node {}
+
+// fn main() {
+//     //testing
+//     io::stdout("Hello world");
+// }
